7a1a284e61bf3277c5c27acb07226413ef594350
Cloud User <centos@ip-172-30-0-81.eu-west-1.compute.internal>
2015-03-30 16:02:12 +0000

    initially add centos7 VM conf

---
+++ centos7/init.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+#install wget
+yum -y install wget
+
+#install vim and similar tools
+yum -y install vim
+wget ftp://195.220.108.108/linux/fedora/linux/development/rawhide/x86_64/os/Packages/v/vifm-0.7.8-1.fc22.x86_64.rpm
+yum -y install vifm-0.7.8-1.fc22.x86_64.rpm
+
