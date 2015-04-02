4d9a17cb1681ec98e150ee48556faa08ec685d2f
Cloud User <centos@ip-172-30-0-81.eu-west-1.compute.internal>
2015-04-02 13:32:12 +0000

    add install of git, zsh, ruby

--- centos7/init.sh
+++ centos7/init.sh
@@ -3,6 +3,9 @@
 #update first
 yum -y update
 
+#install git - this should already be done
+yum -y install git
+
 #install wget
 yum -y install wget
 
@@ -10,4 +13,10 @@ yum -y install wget
 yum -y install vim
 wget ftp://195.220.108.108/linux/fedora/linux/development/rawhide/x86_64/os/Packages/v/vifm-0.7.8-1.fc22.x86_64.rpm
 yum -y install vifm-0.7.8-1.fc22.x86_64.rpm
+
+#install zsh
+yum -y install zsh
+
+#install ruby
+yum -y install ruby
 
