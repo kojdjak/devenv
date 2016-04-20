9b6319251744587fea85c368e4ff57e6ba7a7127
kojdjak <juraj.kojdjak@gmail.com>
2016-04-20 12:36:00 +0000

    commentout vifm

--- centos7/init.sh
+++ centos7/init.sh
@@ -11,8 +11,11 @@ yum -y install wget
 
 #install vim and similar tools
 yum -y install vim
-wget ftp://195.220.108.108/linux/fedora/linux/development/rawhide/x86_64/os/Packages/v/vifm-0.7.8-1.fc22.x86_64.rpm
-yum -y install vifm-0.7.8-1.fc22.x86_64.rpm
+#wget ftp://195.220.108.108/linux/fedora/linux/development/rawhide/x86_64/os/Packages/v/vifm-0.7.8-1.fc22.x86_64.rpm
+#wget http://dl.fedoraproject.org/pub/fedora/linux/development/24/Everything/source/tree/Packages/v/vifm-0.8-1.fc24.src.rpm
+#wget http://dl.fedoraproject.org/pub/fedora/linux/releases/22/Everything/source/SRPMS/v/vifm-0.7.8-1.fc22.src.rpm
+#yum -y install vifm-0.7.8-1.fc22.src.rpm
+
 
 #install zsh
 yum -y install zsh
